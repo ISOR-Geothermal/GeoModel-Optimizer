eb3556517a86d7e437652731aee4e99802c07dcc
rm <rm@isor.is>
2025-02-20 16:02:32 +0000

    public repo

---
+++ geomodel_optimizer/optimizer.py
@@ -0,0 +1,349 @@
+from .utils import nostdout
+from .waiwerarun import WaiweraRun
+import layermesh.mesh as lm
+from pandas import DataFrame
+import json
+import shutil
+from typing import Tuple, Optional, List, Callable
+from pathlib import Path
+from copy import deepcopy
+from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import itertools
+
+
+class LocationOptimizer:
+    def __init__(self, 
+                 base_input: dict, 
+                 workdir: str, 
+                 mesh_path_h5: str,
+                 mesh_path_exo: Optional[str] = None, 
+                 initial_state_path: Optional[str] = None):
+        self._make_dirs(workdir)
+        self.mesh = lm.mesh(mesh_path_h5)
+        initial_base_input = deepcopy(base_input)
+
+        # this call modifies the input to use files in the local directory
+        base_input = self._prepare_inputs(initial_base_input, mesh_path_h5, mesh_path_exo, initial_state_path)
+        self.base_input = base_input
+
+        self.meta: Optional[DataFrame] = None # metadata about runs
+
+        self.source_terms: List[dict] = [] # info about sources added to base params
+        self.sink_terms: List[dict] = [] # info about sinks added to base params
+        self.combined_terms: List[dict] = [] # product of sink and source terms, generated when run files are output
+
+        self.run_params: List[dict] = [] # run parameters in dict form
+        self.run_file_paths: List[str] = [] # path to json files directly run by waiwera
+        self.runs: List[WaiweraRun] = [] # completed runs
+
+    def add_run(self, 
+                source_idx: Optional[int] = None, 
+                sink_idx: Optional[int] = None, 
+                source_rate: Optional[float] = None,
+                sink_rate: Optional[float] = None, 
+                source_enthalpy: float = 84e3,
+                **unused
+                ) -> None:
+        if (source_idx is None) and (sink_idx is None):
+            raise ValueError("TODO both cant be none") # TODO raise warning instead?
+        
+        params = deepcopy(self.base_input)
+        if sink_idx is not None: # sink_idx can be 0
+            assert sink_rate is not None, f"must provide a sink rate along with sink_idx {sink_idx}"
+            self.add_sink(params, sink_idx, sink_rate)
+        
+        if source_idx is not None:
+            assert source_rate is not None, f"must provide a source rate along with source_idx {source_idx}"
+            self.add_source(params, source_idx, source_rate, enthalpy=source_enthalpy)
+        
+        self.run_params.append(params)
+
+    def add_sink(self, params: dict, cell: int | List[int], rate: float) -> None:
+        """
+        Add a sink at the provided cells. Modifies `params` in place.
+        """
+        try:
+            sources = params["source"]
+        except KeyError:
+            sources = []
+        new_source = {
+            "cell": cell,
+            "rate": -abs(rate) # ensure negative
+        }
+        sources.append(new_source)
+        params["sources"] = sources
+
+    def moving_sink(self,
+                rate: float,
+                cells: Optional[List[int]],
+                coordinates_file: Optional[str] = None
+            ) -> None:
+        """
+        TODO
+        Adds sink in every cell provided in `cells` list. 
+        # Every iteration adds a parameter dict to the optimizer object's run_params list. 
+        Every iteration adds an entry to the sink_terms list. When the run list is finalized the product of 
+        the source_terms and sink_terms is used to generate the full list of runs
+        """
+        if coordinates_file:
+            cells = self._cells_from_file(coordinates_file)
+
+        for i, sink_cell in enumerate(cells):
+            sink_meta = {
+                "sink_idx": sink_cell,
+                "sink_rate": rate,
+                "sink_coords": self.mesh.cell[sink_cell].centre,
+                "sink_name": f"sink_{sink_cell}",
+            }
+            self.sink_terms.append(sink_meta)
+
+    def add_source(self, params: dict, cell: int | List[int], rate: float, enthalpy=84.9e3, component="water") -> None:
+        try:
+            sources = params["source"]
+        except KeyError:
+            sources = []
+        new_source = {
+            "cell": cell,
+            "component": component,
+            "enthalpy": enthalpy,
+            "rate": abs(rate)
+        }
+        sources.append(new_source)
+        params["sources"] = sources        
+
+    def moving_source(self, 
+                rate: float,
+                enthalpy: float,
+                cells: Optional[List[int]] = None,
+                component: str = "water",
+                coordinates_file: Optional[str] = None,
+            ) -> None:
+        """
+        Add a simulation with a source in each of the cells defined in `cells`. If a file 
+        is specified with `coordinates_file`, the cells will be read from there instead. The 
+        file should contain three comma separated numbers per line, representing the X, Y and Z
+        coordinates of the source in the mesh in meters.
+        """
+        if coordinates_file:
+            cells = self._cells_from_file(coordinates_file)
+
+        for i, source_cell in enumerate(cells):
+            source_meta = {
+                "source_idx": source_cell,
+                "source_rate": rate,
+                "source_coords": self.mesh.cell[source_cell].centre,
+                "source_enthalpy": enthalpy,
+                "source_name": f"source_{source_cell}",
+                "source_component": component
+            }
+            self.source_terms.append(source_meta)
+
+    def _cells_from_file(self, file_path: str) -> List[int]:
+        """
+        Read coords from file and turn them into cell indexes. Each line in the input file should 
+        contain one 3d coordinate. Ignore lines commented out with "#"
+
+        TODO warn user about duplicate cells? Warn about coordinates mismatch? (cell.centre vs input)
+        """
+        coordinates: List[float] = []
+
+        with open(file_path) as f:
+            for line in f.readlines():
+                if "#" in line:
+                    continue
+                line_coords = tuple([float(i) for i in line.split(",")])
+                coordinates.append(line_coords)
+
+        cell_indexes = self._cells_from_coords(coordinates)
+        return cell_indexes
+
+    def _cells_from_coords(self, coords: List[Tuple[float]]) -> List[int]:
+        cells = []
+        for c in coords:
+            if len(c) != 3:
+                raise ValueError("TODO need 3d coord")
+            cell = self.mesh.find(c)
+            cells.append(cell.index)
+        return cells
+        
+    def _make_dirs(self, workdir: str):
+        wd = Path(workdir)
+        if wd.is_absolute():
+            raise ValueError("TODO path cannot be absolute")
+        wd.mkdir(exist_ok=True, parents=True)
+        json_dir = wd / "json"
+        output_dir = wd / "outputs"
+        input_dir = wd / "inputs"
+        log_dir = wd / "logs"
+
+        json_dir.mkdir(exist_ok=True)
+        output_dir.mkdir(exist_ok=True)
+        input_dir.mkdir(exist_ok=True)
+        log_dir.mkdir(exist_ok=True)
+
+        self.workdir = wd
+        self.json_dir = json_dir
+        self.output_dir = output_dir
+        self.input_dir = input_dir
+        self.log_dir = log_dir
+
+    def _prepare_inputs(self, parameters: dict, mesh_h5_path: str, mesh_exo_path: Optional[str] = None, initial_state_path: Optional[str] = None) -> dict:
+        """
+        Copy all relevant inputs defined in the parameters file to the working directory
+        """
+        if mesh_exo_path is None:
+            mesh_exo_path = Path(parameters["mesh"]["filename"])
+        else:
+            mesh_exo_path = Path(mesh_exo_path)
+
+        if not mesh_exo_path.exists():
+            raise FileNotFoundError("TODO could not find mesh h5 file")
+        shutil.copy(mesh_exo_path, self.input_dir)
+        shutil.copy(mesh_h5_path, self.input_dir)
+
+        relative_mesh_file = self.input_dir / mesh_exo_path.name
+        relative_mesh_file = relative_mesh_file.as_posix() # need to use linux paths for docker image
+        parameters["mesh"]["filename"] = relative_mesh_file
+
+        if initial_state_path is None:
+            initial_state_path = Path(parameters["initial"]["filename"])
+        else:
+            initial_state_path = Path(initial_state_path)
+        
+        if not initial_state_path.exists():
+            raise FileNotFoundError("TODO could not find state file")
+        shutil.copy(initial_state_path, self.input_dir)
+        
+        relative_state_file = self.input_dir / initial_state_path.name
+        relative_state_file = relative_state_file.as_posix() # need to use linux paths for docker image
+        parameters["initial"]["filename"] = relative_state_file # TODO check if original initial params need to be removed
+
+        base_parameters_output = self.input_dir / "base_params.json"
+        with base_parameters_output.open("w") as f:
+            json.dump(parameters, f, indent=4)
+        return parameters
+        
+    def _get_combined_terms(self) -> List[dict]:
+        """
+        If the optimization has both moving sinks and sources, the total number of runs is 
+        the product of all source locations and all sink locations
+        """
+        # if only either source or sink terms exist nothing needs to be done
+        if len(self.source_terms) == 0:
+            return self.sink_terms
+        if len(self.sink_terms) == 0:
+            return self.source_terms
+        
+        # else we need to create a product of all terms
+        combined_terms = []
+        for source, sink in itertools.product(self.source_terms, self.sink_terms):
+            combined = source.copy()
+            combined.update(sink)
+            combined_terms.append(combined)
+
+        return combined_terms
+
+    def _get_run_name(self, meta: dict) -> str:
+        sink_name = meta.get("sink_name", "")
+        source_name = meta.get("source_name", "")
+
+        name = f"{sink_name}_{source_name}"
+        if name.startswith("_"):
+            name = name[1:]
+        if name.endswith("_"):
+            name = name[:-1]
+        return name
+
+    def output_run_files(self):
+        """
+        Set the run parameters outputs and output the parameters to a json file to be 
+        used by Waiwera. Also creates the self.meta DataFrame that holds information
+        about run parameters
+        """
+        self.combined_terms = self._get_combined_terms()
+        for terms in self.combined_terms:
+            self.add_run(**terms)
+
+        run_file_paths = []
+        metadata = [] # gather meta information to be made into a dataframe
+        for i, (params, meta) in enumerate(zip(self.run_params, deepcopy(self.combined_terms))):
+            name = self._get_run_name(meta)
+            self._set_output(params, name)
+            json_path = self.json_dir / (name + ".json")
+            with json_path.open("w") as f:
+                json.dump(params, f, indent=4)
+            run_file_paths.append(str(json_path).replace("\\", "/"))
+
+            meta.update({
+                "json_file": str(json_path),
+                "run_index": i,
+                "run_name": name,
+                "loss": None
+            })
+            metadata.append(meta)
+
+        self.run_file_paths = run_file_paths
+        self.meta = self._generate_info_df(metadata)
+
+    def _generate_info_df(self, meta: List[dict]) -> DataFrame:
+        df = DataFrame(meta)
+        df["source_x"] = df.source_coords.apply(lambda x: x[0])
+        df["source_y"] = df.source_coords.apply(lambda x: x[1])
+        df["source_z"] = df.source_coords.apply(lambda x: x[2])
+        del df["source_coords"]
+        
+        df["sink_x"] = df.sink_coords.apply(lambda x: x[0])
+        df["sink_y"] = df.sink_coords.apply(lambda x: x[1])
+        df["sink_z"] = df.sink_coords.apply(lambda x: x[2])
+        del df["sink_coords"]
+
+        return df
+
+    def _set_output(self, params: dict, basename: str):
+        h5_name = self.output_dir / (basename + ".h5")
+        log_name = self.log_dir / (basename + ".yaml")
+        params["output"]["filename"] = h5_name.as_posix()
+        params["logfile"]["filename"] = log_name.as_posix()
+
+    def _sequential_run(self, nproc=4) -> bool:
+        for path in tqdm(self.run_file_paths):
+            run = self._single_run(path, nproc=nproc)
+            self.runs.append(run)
+        return True
+
+    def _single_run(self, path: str, nproc: int = 4) -> WaiweraRun:
+        with nostdout():
+            run = WaiweraRun.run_from_file(path, num_processes=nproc)
+        return run
+
+    def _parallel_run(self, pool_size=4, nproc=1):
+        # TODO
+        with tqdm(total=len(self.run_params)) as progress:
+            with ProcessPoolExecutor(max_workers=pool_size) as ppe:
+                futures = [ppe.submit(self._single_run, path, nproc) for path in self.run_file_paths]
+                for future in as_completed(futures):
+                    progress.update(1)
+
+    def list_run_output_files(self) -> List[Path]: # TODO drop?
+        return list(self.output_dir.glob("*.h5"))
+
+    def compute_loss(self, loss_function: Callable):
+        if self.meta is None:
+            raise ValueError("TODO no meta")
+        calculated_loss = []
+        for run in self.runs:
+            loss = run.execute_function(loss_function)
+            calculated_loss.append(loss)
+        self.meta["loss"] = calculated_loss
+    
+    def __len__(self):
+        return len(self.run_params)
+    
+    def execute(self, nproc=4):
+        """
+        Perform a run with each set of parameters in `self.run_params`. The runs can then be accessed 
+        from the self.runs list
+        """
+        self.output_run_files()
+        self._sequential_run(nproc=nproc)
---
+++ geomodel_optimizer/waiwerarun.py
@@ -0,0 +1,148 @@
+import layermesh.mesh as lm
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import pywaiwera
+import json
+import shutil
+from typing import Tuple, Optional, List, Callable
+import os, sys
+from pathlib import Path
+from copy import deepcopy
+from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import contextlib
+import time
+from pandas import Timestamp
+
+
+class WaiweraRun:
+    def __init__(self, params, json_file, temperature, pressure, timestamp, meta: dict | None = None):
+        # self.params = params.copy()
+        self.params = deepcopy(params)
+        self.json_file = json_file
+        self.temperature = temperature.copy()
+        self.pressure = pressure.copy()
+        self.timestamp = timestamp
+        
+        # keep some meta information about the specifics of this run
+        self.meta = {}
+        if meta is not None:
+            self.meta.update(meta)
+
+        mesh_path = Path(params["mesh"]["filename"])
+        if not mesh_path.exists():
+            raise FileNotFoundError(f"Could not find the mesh file at {mesh_path}. \
+                                    Ensure that the file exists at the location specified in the passed parameters")
+        self.mesh = self._load_mesh(mesh_path)
+        self.h5_path = Path(params["output"]["filename"])
+        if not self.h5_path.exists():
+            raise Warning(f"could not find corresponding h5 file {self.h5_path}. This could just mean the run has not \
+                          happened yet, or it could mean the path specified is wrong")
+
+    @classmethod
+    def run_from_file(cls, json_file: str, num_processes=2):
+        timestamp = Timestamp.now()
+
+        env = pywaiwera.docker.DockerEnv()
+        env.run_waiwera(json_file, noupdate=True, num_processes=num_processes)
+        
+        with open(json_file, "r") as f:
+            params = json.load(f)
+
+        h5_name = params["output"]["filename"]
+        t = cls.get_temperature(h5_name, -1)
+        p = cls.get_pressure(h5_name, -1)
+
+        run = WaiweraRun(params, json_file, t, p, timestamp)
+        return run
+
+    @classmethod
+    def run_from_dict(cls, params: dict, json_file: str, num_processes=2):
+        timestamp = Timestamp.now()
+        with open(json_file, "w") as f:
+            json.dump(params, f, indent=4)
+            
+        env = pywaiwera.docker.DockerEnv()
+        env.run_waiwera(json_file, noupdate=True, num_processes=num_processes)
+        
+        h5_name = params["output"]["filename"]
+        t = cls.get_temperature(h5_name, -1)
+        p = cls.get_pressure(h5_name, -1)
+
+        run = WaiweraRun(params, json_file, t, p, timestamp)
+        return run
+
+    @classmethod
+    def from_file(cls, h5_filename: str, json_filename: str):
+        with open(json_filename) as f:
+            params = json.load(f)
+        t = cls.get_temperature(h5_filename, timestep=-1)
+        p = cls.get_pressure(h5_filename, timestep=-1)
+        timestamp = Timestamp.now()
+
+        run = WaiweraRun(params, json_filename, t, p, timestamp)
+        return run
+
+    def plots(self, value: str = "temperature", depth=0):
+        if value in ["temperature", "t"]:
+            data = self.temperature
+        elif value in ["pressure", "p"]:
+            data = self.pressure
+        else:
+            raise ValueError
+            
+        fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 4))
+        self.mesh.slice_plot("x", axes=ax1, value=data)
+        self.mesh.layer_plot(depth, value=data, axes=ax2)
+    
+    def execute_function(self, function: Callable) -> 'Undefined':
+        """
+        Execute a callable `function` that takes an open h5py.File as input
+        """
+        with h5py.File(str(self.h5_path)) as data:
+            return function(data)
+    
+    def _load_mesh(self, mesh_path: Path) -> 'layermesh.Mesh':
+        if mesh_path.suffix == ".exo":
+            mesh_filename = str(mesh_path).replace(".exo", ".h5")
+        else:
+            mesh_filename = str(mesh_path)
+        print(mesh_filename)
+        mesh = lm.mesh(str(mesh_filename))
+        return mesh
+
+    def __repr__(self):
+        r = f"Run [{self.timestamp}]"
+        return r
+    
+    @staticmethod
+    def get_values(value: str, filename: str, timestep: int) -> np.array:
+        with h5py.File(filename) as data:
+            cell_idx = [int(i[0]) for i in data["cell_index"]]
+            temperature = data["cell_fields"][value][timestep]
+            sorted_temp = np.zeros(temperature.shape)
+            for i, idx in enumerate(cell_idx):
+                sorted_temp[i] = temperature[idx]
+        return sorted_temp
+
+    @staticmethod
+    def get_temperature(filename: str, timestep: int):
+        return WaiweraRun.get_values("fluid_temperature", filename, timestep)
+    
+    @staticmethod
+    def get_pressure(filename: str, timestep: int):
+        return WaiweraRun.get_values("fluid_pressure", filename, timestep)
+
+
+class NullOutput:
+    def write(self, x):
+        pass
+
+
+@contextlib.contextmanager
+def nostdout():
+    keep = sys.stdout
+    sys.stdout = NullOutput()
+    yield
+    sys.stdout = keep
